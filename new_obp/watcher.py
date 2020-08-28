c17b0cce0c409a2bb9c50c9932f6d6b6313a014c
MaximSurovtsev <gr4m0zeka@gmail.com>
2020-08-28 13:09:59 +0300

    1

---
+++ new_obp/watcher.py
@@ -0,0 +1,47 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time 
+import os 
+
+
+
+
+class Handler(FileSystemEventHandler):
+	def on_created(self, event):
+		print (event)
+
+	def on_deleted(self, event):
+		print (event)
+
+	def on_modified(self, event):
+		if '.html' in str(event):
+			with open('header.html', 'r') as file:
+				header = file.read()
+			with open('footer.html', 'r') as file:
+				footer = file.read()
+
+			for name in os.listdir():
+				if '.html' in name and name not in ['footer.html', 'header.html']:
+					with open(name, 'r') as file:
+						html = file.read()
+					with open('dist/' + name, 'w') as file:
+						file.write(html.replace('{ header }', header).replace('{ footer }', footer))
+
+
+		print(event)
+
+	def on_moved(self, event):
+		print (event)
+
+
+
+observer = Observer()
+observer.schedule(Handler(), path='/Users/maximsurovtsev/new_obp/', recursive=False)
+observer.start()
+
+try:
+    while True:
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
